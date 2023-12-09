a03f0bdd6562dae57fd3af39f03bdad39a3f4196
MariaSoler <soler1998carlota@gmail.com>
2023-12-09 02:26:46 -0600

    resources/views/dash/delete.blade.php

--- resources/views/dash/delete.blade.php
+++ resources/views/dash/delete.blade.php
@@ -5,18 +5,18 @@
       @csrf
       @method('DELETE')
     <div class="modal-content">
-      <div class="modal-header">
+    <div class="modal-header" style="background-color: #8B1E06 !important;">
         <h5 class="modal-title" id="exampleModalLabel">Eliminacion De Registro</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        Deseas eliminar el registro {{$planillas['COD_PLANILLA']}}
+        Deseas eliminar el registro? {{$planillas['COD_PLANILLA']}}
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Cerrar</button>
-        <a href="{{url('planillas/delete',$planillas['COD_PLANILLA'])}}"><button class="btn btn-primary btn-sm">ELIMINAR</button></a>
+        <a href="{{url('planillas/delete',$planillas['COD_PLANILLA'])}}"><button class="btn btn-primary btn-sm">Eliminar</button></a>
       </div>
     </div>
     </form>
