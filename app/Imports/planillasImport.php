9ee2dd793721784d1d5dfca7875fab3729ba9a4a
MariaSoler <soler1998carlota@gmail.com>
2023-12-09 02:29:13 -0600

    app/Imports/planillasImport.php

--- app/Imports/planillasImport.php
+++ app/Imports/planillasImport.php
@@ -21,19 +21,21 @@ class planillasImport implements ToModel
        $cell = $row[2];
 
        // Convert the Excel serial number to a date
-       $fechaPago = Carbon::createFromTimestamp(($row[2] - 25569) * 86400);
+       $fechaPago = Carbon::createFromTimestamp(($row[4] - 25569) * 86400);
 
 // Convertir la fecha de registro a la fecha y hora actuales
 $fechaRegistro = Carbon::today();
 
                 return new Planillas([
-                    'COD_PLANILLA'    => $row[0],
+                    'COD_PLANILLA'    => (int)$row[0], // Convertir a entero
                     'COD_AFILIADO'    => $row[1],
-                     'FEC_PAGO'       => $fechaPago,
-                    'DENOMINACION'    => $row[3],
-                    'VAL_PAGADO'      => $row[4],
-                    'VAL_APO_MENSUAL' => $row[5],
-                    'USR_REGISTRO'    => $row[6],
+                    'NUM_AFILIADO'    => $row[2],
+                    'NOM_COMPLETO'    => $row[3],
+                    'FEC_PAGO'       => $fechaPago,
+                    'DENOMINACION'    => $row[5],
+                    'VAL_PAGADO'      => $row[6],
+                    'VAL_APO_MENSUAL' => $row[7],
+                    'USR_REGISTRO'    => $row[8],
                     'FEC_REGISTRO'    => $fechaRegistro, 
                 ]);
             }
