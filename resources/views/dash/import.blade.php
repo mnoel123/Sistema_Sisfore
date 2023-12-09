3a8502bce6d539f98fa997aa65a16e329ceea1ef
MariaSoler <soler1998carlota@gmail.com>
2023-12-09 02:24:27 -0600

    .

--- resources/views/dash/import.blade.php
+++ resources/views/dash/import.blade.php
@@ -2,7 +2,7 @@
  <div class="modal fade" id="Modal-import-{{('import') }}" tabindex="-1" role="dialog" aria-labelledby="importarPlanillaModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                    <div class="modal-header">
+                    <div class="modal-header" style="background-color: #8B1E06 !important;">
                         <h5 class="modal-title" id="importarPlanillaModalLabel">Importar Planilla</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
--- resources/views/dash/planillas.blade.php
+++ resources/views/dash/planillas.blade.php
@@ -22,6 +22,8 @@
                     <tr>
                         <th>CODIGO PLANILLA</th>
                         <th>CODIGO AFILIADO</th>
+                        <th>NUMERO AFILIADO</th>
+                        <th>NOMBRE COMPLETO</th>
                         <th>FECHA DE PAGO</th>
                         <th>DENOMINACION</th>
                         <th>VALOR PAGADO</th>
@@ -37,6 +39,8 @@
                         <tr>
                             <td class="inner-table text-center">{{$planillas["COD_PLANILLA"]}}</td>
                             <td class="inner-table text-center">{{$planillas["COD_AFILIADO"]}}</td>
+                            <td class="inner-table text-center">{{$planillas["NUM_AFILIADO"]}}</td>
+                            <td class="inner-table text-center">{{$planillas["NOM_COMPLETO"]}}</td>
                             <td class="inner-table text-center">{{$planillas["FEC_PAGO"]}}</td>
                             <td class="inner-table text-center">{{$planillas["DENOMINACION"]}}</td>
                             <td class="inner-table text-center">{{$planillas["VAL_PAGADO"]}}</td>
@@ -63,18 +67,35 @@
 </div>
 </div>
 @endsection
-
-@section('css')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
-@endsection
-
 @section('js')
-    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
-
-
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.8/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.8/js/responsive.bootstrap4.min.js"></script>
     <script>
-      
+        $(document).ready(function() {
+            $('#planillas').DataTable({
+                responsive: true,
+                autoWidth: false,
+                "language": {
+                    "lengthMenu": "Registros por página MENU ",
+                    "zeroRecords": "No se encontro registro",
+                    "info": "Mostrando la página PAGE de PAGES",
+                           "search": "Buscar",
+                    "paginate": {
+                        "previous": "Anterior",
+                        "next": "Siguiente"
+                    },
+                    "infoEmpty": "No hay registros",
+                    "infoFiltered": "(Filtrado de MAX registros totales)"
+                },
+                "lengthMenu": [
+                    [5, 10, 50, 1],
+                    [5, 10, 50, "All"]
+                ]
+
+            });
+        });
     </script>
 @endsection
